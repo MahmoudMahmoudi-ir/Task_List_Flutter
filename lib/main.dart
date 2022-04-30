56e6aef8ab5556cbd54ac2c2fe5f1a4eda5b4f00
Mahmoud Mahmoudi <mahmoudmahmoudi.ir@gmail.com>
2022-04-30 23:13:02 +0430

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:task_list/data/data.dart';
+import 'package:task_list/data/repo/repository.dart';
+import 'package:task_list/data/source/hive_task_source.dart';
+import 'package:task_list/screens/home/home.dart';
+
+const taskBoxName = 'tasks';
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
+  Hive.registerAdapter(PriorityAdapter());
+  await Hive.openBox<TaskEntity>(taskBoxName);
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: primaryVariantColor));
+  runApp(ChangeNotifierProvider<Repository<TaskEntity>>(
+      create: (context) =>
+          Repository<TaskEntity>(HiveTaskDataSource(Hive.box(taskBoxName))),
+      child: const MyApp()));
+}
+
+const Color primaryColor = Color(0xff794CFF);
+const Color primaryVariantColor = Color(0xff5C0AFF);
+const secondaryTextColor = Color(0xffAFBED0);
+const normalPriority = Color(0xffF09819);
+const lowPriority = Color(0xff3BE1F1);
+const highPriority = primaryColor;
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    const primaryTextColor = Color(0xff1D2830);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          textTheme: GoogleFonts.poppinsTextTheme(const TextTheme(
+              headline6: TextStyle(fontWeight: FontWeight.bold))),
+          inputDecorationTheme: const InputDecorationTheme(
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              labelStyle: TextStyle(color: secondaryTextColor),
+              border: InputBorder.none,
+              iconColor: secondaryTextColor),
+          colorScheme: const ColorScheme.light(
+              primary: primaryColor,
+              primaryVariant: primaryVariantColor,
+              background: Color(0xffF3F5F8),
+              onSurface: primaryTextColor,
+              onPrimary: Colors.white,
+              onBackground: primaryTextColor,
+              secondary: primaryColor,
+              onSecondary: Colors.white)),
+      home: HomeScreen(),
+    );
+  }
+}
